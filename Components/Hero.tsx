76dfb3c31ef21b4cfe71936aa27ba2558807159a
Mohammad Alsallal <malslal787@gmail.com>
2024-08-17 02:45:09 +0300

    small changes

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -53,14 +53,14 @@ const Hero = () => {
         </div>
       </div>
       <div className="relative flex flex-1 items-start">
-        <div className="relative z-20 w-[268px] flex flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
+        <div className="relative z-20 w-[268px] flex flex-col rounded-3xl bg-green-90 px-7 py-8">
           <div className="flex flex-col">
             <div className="flexBetween">
               <p className="regular-16 text-gray-20">Location</p>
               <Image src="/close.svg" alt="close" width={24} height={24} />
             </div>
             <p className="bold-20 text-white">Aguas Calientes</p>
-            <div className="flexBetween">
+            <div className="flexBetween mt-8">
               <div className="felx flex-col">
                 <p className="regular-16 block text-gray-20">Distance</p>
                 <p className="bold text-white">173.28 mi</p>
